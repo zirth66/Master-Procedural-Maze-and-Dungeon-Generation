de40e1c3f9f7c38d95705e10a737efa00587aa2c
zirth <zirth66@gmail.com>
2021-01-10 18:32:20 +0100

    Added package for first person character

--- Assets/Maze.cs
+++ Assets/Maze.cs
@@ -1,8 +1,8 @@
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class MapLocation
+public class MapLocation       
 {
     public int x;
     public int z;
@@ -15,52 +15,43 @@ public class MapLocation
 }
 
 public class Maze : MonoBehaviour
-{  
-    [Range(0, 100)]
-    public int width = 30; // x length
-    [Range(0, 100)]
-    public int depth = 30; // y length
+{
+    public int width = 30; //x length
+    public int depth = 30; //z length
     public byte[,] map;
     public int scale = 6;
 
     // Start is called before the first frame update
     void Start()
     {
-        InitialiseMap(); // Sets everthing to walls
-        Generate(); // Digs corridors
+        InitialiseMap();
+        Generate();
         DrawMap();
     }
+
     void InitialiseMap()
     {
-        map = new byte[width, depth];
+        map = new byte[width,depth];
         for (int z = 0; z < depth; z++)
-        {
             for (int x = 0; x < width; x++)
             {
-                    map[x, z] = 1; // 1 = wall, 0 = corridor           
+                    map[x, z] = 1;     //1 = wall  0 = corridor
             }
-        }
     }
 
-    public virtual void Generate() // public virtual so we can ovveride it in Crawler.cs
+    public virtual void Generate()
     {
         for (int z = 0; z < depth; z++)
-        {
             for (int x = 0; x < width; x++)
             {
-
-                if (Random.Range(0, 100) < 50)
-                {
-                    map[x, z] = 0; // 1 = wall, 0 = corridor    
-                }
+               if(Random.Range(0,100) < 50)
+                 map[x, z] = 0;     //1 = wall  0 = corridor
             }
-        }
     }
 
     void DrawMap()
     {
         for (int z = 0; z < depth; z++)
-        {
             for (int x = 0; x < width; x++)
             {
                 if (map[x, z] == 1)
@@ -71,8 +62,8 @@ public class Maze : MonoBehaviour
                     wall.transform.position = pos;
                 }
             }
-        }
     }
+
     // Update is called once per frame
     void Update()
     {
